a38bbb94d2325f33386627ee2d982687cfb8a5f2
Tim <38327800+Str4ky@users.noreply.github.com>
2023-05-29 13:22:44 +0200

    Update script.js

--- ctgp7_updater/script.js
+++ ctgp7_updater/script.js
@@ -1,46 +1,39 @@
-const https = require('https');
-const fs = require('fs');
-const { exec } = require('child_process');
-
-const url = 'https://github.com/PabloMK7/CTGP-7updates/archive/master.zip';
-
-function downloadFile(url, destination, callback) {
-  const file = fs.createWriteStream(destination);
-
-  https.get(url, (response) => {
-    response.pipe(file);
-
-    file.on('finish', () => {
-      file.close(callback);
+function downloadFile(url, filename, callback) {
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      // Enregistrer le fichier localement
+      const file = new File([blob], filename);
+      saveAs(file);
+      callback(null);
+    })
+    .catch(error => {
+      callback(error);
     });
-  }).on('error', (err) => {
-    fs.unlink(destination);
-    if (callback) callback(err.message);
-  });
 }
 
 function handleFolderSelection(event) {
   event.preventDefault();
   const input = document.getElementById('folderInput');
-  const selectedFolder = input.files[0].path;
+  const selectedFolder = input.files[0];
 
-  const destination = `${selectedFolder}/CTGP-7updates.zip`;
+  const destination = selectedFolder.webkitRelativePath || selectedFolder.name;
+  const filename = 'CTGP-7updates.zip';
+  const downloadUrl = 'https://github.com/PabloMK7/CTGP-7updates/archive/master.zip';
 
-  downloadFile(url, destination, (error) => {
+  downloadFile(downloadUrl, filename, (error) => {
     if (error) {
       console.error(`Une erreur s'est produite lors du téléchargement : ${error}`);
     } else {
       console.log('Le fichier a été téléchargé avec succès !');
-      console.log(`Dossier de destination : ${selectedFolder}`);
+      console.log(`Dossier de destination : ${destination}`);
 
-      exec(`unzip ${destination} -d ${selectedFolder}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Une erreur s'est produite lors de l'extraction du fichier zip : ${error}`);
-        } else {
-          console.log('Le fichier zip a été extrait avec succès !');
-          // Faites quelque chose avec les fichiers extraits
-        }
-      });
+      const unzipCommand = `unzip ${filename} -d ${destination}`;
+
+      // Exécuter des opérations supplémentaires après le téléchargement
+      // Par exemple, vous pouvez utiliser l'API File et l'API FileReader pour lire le contenu des fichiers extraits
+
+      console.log('Commande d\'extraction :', unzipCommand);
     }
   });
 }
