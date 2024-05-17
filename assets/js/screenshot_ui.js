39fd8e939f675f9b9803fa06a4c45e80df2c81ba
Tim <38327800+Str4ky@users.noreply.github.com>
2024-05-17 23:40:10 +0200

    added clipboard function

--- assets/js/screenshot_ui.js
+++ assets/js/screenshot_ui.js
@@ -14,3 +14,29 @@ function screenshot() {
     });
 
 }
+
+function clipboard() {
+    let container = document.querySelector(".image-container");
+    let clipboard = document.getElementById('clipboard');
+    let images = container.querySelectorAll('img');
+
+    for (let img of images) {
+        if (!img.src) {
+            img.src = '../../assets/img/blank.png';
+        }
+    }
+
+    clipboard.innerHTML = '<i class="fa-solid fa-check"></i> Done';
+    setTimeout(() => {
+        clipboard.innerHTML = '<i class="fa-solid fa-clipboard"></i> Copy render';
+    }, 2000);
+
+    domtoimage.toBlob(container)
+        .then(function(blob) {
+            navigator.clipboard.write([
+                new ClipboardItem({
+                    'image/png': blob
+                })
+            ])
+        });
+}
